fabec7825a556863e15eb9b4a445f5c2616c6d75
ibbibrahim <ibrahimkashif97@gmail.com>
2025-09-27 11:12:36 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$serverName = "tcp:sqlserver-lab.database.windows.net,1433";  // full server name
+$connectionOptions = array(
+    "Database" => "sqldb-lab",                   // your DB name
+    "Uid" => "adminibrahim",       // admin user + @server
+    "PWD" => "Ibrahim@123"                       // password
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if ($conn) {
+    echo "✅ Connection successful!";
+    sqlsrv_close($conn);
+} else {
+    echo "❌ Connection failed!";
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
